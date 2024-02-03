f021e973aad4fe3c43b504c2b2756c2c362d592e
Pearcep <pearce.pelia@gmail.com>
2024-02-02 16:46:42 -0800

    new

--- 31fizzbuzz.py
+++ 31fizzbuzz.py
@@ -1,7 +1,7 @@
 # 31fizzbuzz by Pearce Pelia
 
-for i in range(1, 26):
-	if i % 15 == 0: print('fizzbuzz')
+for i in range(1, 100):
+	if i % 3 == 0 and i % 5 == 0: print('fizzbuzz')
 	elif i % 5 == 0: print('buzz')
 	elif i % 3 == 0: print('fizz')
 	else: print(i)
