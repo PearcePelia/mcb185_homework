64fa9b59d45f6e59ddcb3e00e9feed666d90cfc5
Pearcep <pearce.pelia@gmail.com>
2024-02-10 15:05:28 -0800

    new

--- 46savingthrows.py
+++ 46savingthrows.py
@@ -1,18 +1,33 @@
 #46savingthrows.py by Pearce Pelia
+#not correct 
 
 import random
-
-#DC of 5
 limit = 1000
 advantage = 0
 disadvantage = 0
-result = 0
+result1 = 0
+
+for i in range(limit):
+	for j in range(1):
+	d1 = random.randint(1, 20)
+	d2 = random.randint(1, 20)
+	if d1 > d2: 
+		result1 += d1
+	else: 
+		result1 += d2
+	
+#DC of 10
 for i in range(limit):
 	print(random.randint(1, 2))
-	if result == 1: print(advantage)
-	elif result == 2: print(disadvantage)
-
-#DC of 10
+	if result2 == 1: print(advantage)
+	elif result2 == 2: print(disadvantage)
 
 #DC of 15
+for i in range(limit):
+	print(random.randint(1, 2))
+	if result3 == 1: print(advantage)
+	elif result3 == 2: print(disadvantage)
 	
+print(f'DC of 5 - {result1 / limit}')
+print(f'DC of 10 - {result2 / limit}')
+print(f'DC of 15 - {result3 / limit}')
--- 48report.md
+++ 48report.md
@@ -2,10 +2,10 @@
 
 print()
 print('45dndstats.py')
-print('+#3D6 - 10.4756%\n+#3D6r1 - 11.7524%\n+#3D6x2 - 13.4346%\n+#4D6d1 - 12.2939%')
+print('+3D6 - 10.4756%\n+3D6r1 - 11.7524%\n+3D6x2 - 13.4346%\n+4D6d1 - 12.2939%')
 print()
-print('46savingthrows.py')
-print('loading...')
+print('46savingthrows.py (professor avg)')
+print('DC\tnorm\tadv\tdis\n5\t0.795\t0.960\t0.638\n10\t0.549\t0.799\t0.307\n15\t0.295\t0.502\t0.084')
 print()
 print('47deathsaves.py')
 print('+dying - 0.41%\n+stable - 0.4126%\n+revive - 0.1774%')
